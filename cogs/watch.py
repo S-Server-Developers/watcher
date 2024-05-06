af334d961fe7351c02080752b32c3cdb66ffdffa
aic-6301 <rx435423@gmail.com>
2024-05-06 22:51:42 +0900

    ダウン通知完成形

--- cogs/watch.py
+++ cogs/watch.py
@@ -28,17 +28,22 @@ class watcher(commands.Cog):
 
     @commands.Cog.listener()
     async def on_presence_update(self, before, after: discord.Member):
-        if after.id == '935855687400054814':
-            if after.status.offline:
-                embed = discord.Embed(title="ダウン通知", description="Earthquickly Botがダウンしてしまいました！", color=discord.Color.red())
+        member:discord.User = self.bot.get_user(935855687400054814)
+        if after.id == member.id:
+            if after.status == discord.Status.offline:
+                embed = discord.Embed(title="ダウン通知", description=f"{after.display_name}がダウンしてしまいました！", color=discord.Color.red())
                 self.message = await self.bot.get_channel(1236680579454603285).send(embed=embed)
             else:
                 if self.message:
-                    embed = discord.Embed(title="アップ通知", description="Earthquickly Botが復活しました！", color=discord.Color.green())
-                    self.message.edit(embed=embed)
+                    embed = discord.Embed(title="アップ通知", description=f"{after.display_name}が復活しました！", color=discord.Color.green())
+                    await self.message.edit(embed=embed)
                     self.message = None
                 else:
                     return
+    
+    async def cog_unload(self):
+        self.watcher.stop()
+
 
 async def setup(bot: commands.Bot):
     await bot.add_cog(watcher(bot))
